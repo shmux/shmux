a8bd3b26d124a416f1d460bd73af862e3f6da9d5
Christophe Kalt <kalt@taranis.org>
2003-04-26 01:39:51 +0000

    addition of siglist.[ch] and signals.awk

---
+++ src/siglist.c
@@ -0,0 +1,41 @@
+/*
+** Copyright (C) 2003 Christophe Kalt
+**
+** This file is part of shmux,
+** see the LICENSE file for details on your rights.
+*/
+
+#include "os.h"
+
+#if defined(HAVE_DECL_SYS_SIGNAME)
+# include <signal.h>
+#endif
+
+#include "siglist.h"
+#include "signals.h"
+
+static char const rcsid[] = "@(#)$Id: siglist.c,v 1.1 2003-04-26 01:39:50 kalt Exp $";
+
+int
+getsignumbyname(name)
+char *name;
+{
+    int i;
+
+    i = 0;
+#if defined(HAVE_SYS_SIGNAME)
+    while (i < NSIG)
+      {
+	if (strcmp(sys_signame[i], name) == 0)
+	    return i;
+#else
+    while (signame[i].name != NULL)
+      {
+	if (strcmp(signame[i].name, name) == 0)
+	    return signame[i].num;
+#endif
+	i += 1;
+      }
+    return -1;
+}
+
---
+++ src/siglist.h
@@ -0,0 +1,15 @@
+/*
+** Copyright (C) 2003 Christophe Kalt
+**
+** This file is part of shmux
+** see the LICENSE file for details on your rights.
+**
+** $Id: siglist.h,v 1.1 2003-04-26 01:39:50 kalt Exp $
+*/
+
+#if !defined(_SIGLIST_H_)
+# define _SIGLIST_H_
+
+int getsignumbyname(char *);
+
+#endif
