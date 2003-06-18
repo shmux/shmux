170c0beced0b35b3ec568a5a705f12b7e5505573
Christophe Kalt <kalt@taranis.org>
2003-06-18 00:26:29 +0000

    OUT_NULL can't be 0

--- src/loop.h
+++ src/loop.h
@@ -4,15 +4,15 @@
 ** This file is part of shmux
 ** see the LICENSE file for details on your rights.
 **
-** $Id: loop.h,v 1.9 2003-05-08 00:27:58 kalt Exp $
+** $Id: loop.h,v 1.10 2003-06-18 00:26:29 kalt Exp $
 */
 
 #if !defined(_LOOP_H_)
 # define _LOOP_H_
 
-#define OUT_NULL  0x00	/* Used internally by loop() */
-#define OUT_MIXED 0x01	/* Mixed target output */
-#define OUT_ATEND 0x02	/* Non-mised target output (displayed at end) */
+#define OUT_NULL  0x01	/* Used internally by loop() */
+#define OUT_MIXED 0x02	/* Mixed target output */
+#define OUT_ATEND 0x04	/* Non-mised target output (displayed at end) */
 #define OUT_COPY  0x10	/* Keep copies of everything for the user */
 #define OUT_IFERR 0x20	/* Output only displayed on error */
 #define OUT_ERR   0x40	/* Error found in output */
