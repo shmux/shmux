579119858a3abb2554ab11e7e01df587eaaab04e
Christophe Kalt <kalt@taranis.org>
2004-03-25 01:41:29 +0000

    comment typo, no code change.

--- src/loop.h
+++ src/loop.h
@@ -1,10 +1,10 @@
 /*
-** Copyright (C) 2002, 2003 Christophe Kalt
+** Copyright (C) 2002, 2003, 2004 Christophe Kalt
 **
 ** This file is part of shmux
 ** see the LICENSE file for details on your rights.
 **
-** $Id: loop.h,v 1.11 2003-11-08 01:17:27 kalt Exp $
+** $Id: loop.h,v 1.12 2004-03-25 01:41:29 kalt Exp $
 */
 
 #if !defined(_LOOP_H_)
@@ -12,7 +12,7 @@
 
 #define OUT_NULL  0x01	/* Used internally by loop() */
 #define OUT_MIXED 0x02	/* Mixed target output */
-#define OUT_ATEND 0x04	/* Non-mised target output (displayed at end) */
+#define OUT_ATEND 0x04	/* Non-mixed target output (displayed at end) */
 #define OUT_COPY  0x10	/* Keep copies of everything for the user */
 #define OUT_IFERR 0x20	/* Output only displayed on error */
 #define OUT_ERR   0x40	/* Error found in output */
